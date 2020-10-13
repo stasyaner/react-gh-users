2396c8f3035a84f9caeaa9e5435ac6457df680e9
stasyaner <stasyaner@hotmail.com>
2020-10-13 17:37:06 +0200

    some basic page setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
 import React from "react";
-import logo from "./assets/logo.svg";
-import "./style/App.css";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+} from "react-router-dom";
+import Home from "./Home";
 
 const App: React.FC = () => (
-    <div className="App">
-        <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <p>
-        Edit <code>src/App.tsx</code> and save to reload.
-            </p>
-            <a
-                className="App-link"
-                href="https://reactjs.org"
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-        Learn React
-            </a>
-        </header>
-    </div>
+    <Router>
+        <Switch>
+            <Route exact path="/">
+                <Home />
+            </Route>
+            {/* <Route path="/topics">
+                <Topics />
+            </Route> */}
+        </Switch>
+    </Router>
 );
 
 export default App;
---
+++ src/Home.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+
+const Home: React.FC = () => (
+    <Container className="my-5">
+        <h3>home</h3>
+    </Container>
+);
+
+export default Home;
