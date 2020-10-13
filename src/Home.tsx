4905f4a9f5eb38efc0affb18fd1fc2d93a1dbc91
stasyaner <stasyaner@hotmail.com>
2020-10-13 17:46:45 +0200

    move container from home to app

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,10 +1,7 @@
 import React from "react";
-import { Container } from "react-bootstrap";
 
 const Home: React.FC = () => (
-    <Container className="my-5">
-        <h3>home</h3>
-    </Container>
+    <h3>home</h3>
 );
 
 export default Home;
