8a57f41412a7916adcf1a8b4e72255824b508034
stasyaner <stasyaner@hotmail.com>
2020-10-13 16:46:52 +0200

    reorganize default folder structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import logo from "../assets/logo.svg";
+import "../style/App.css";
 
 const App: React.FC = () => (
     <div className="App">
