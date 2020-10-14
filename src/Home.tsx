3c191095c1ad47d72eec0dbad90245c782bda4da
stasyaner <stasyaner@hotmail.com>
2020-10-14 18:47:52 +0200

    Home: Pagination into a feature

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,35 +1,11 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import Pagination from "./features/pagination/Pagination";
 import UserList from "./features/userList/UserList";
 
 const Home: React.FC = () => (
     <>
         <UserList />
-        <nav
-            className="d-flex justify-content-center mt-2"
-            aria-label="Page navigation example"
-        >
-            <ul className="pagination text-center">
-                <li className="page-item disabled">
-                    <button className="page-link" aria-label="Previous">
-                        <span aria-hidden="true">&laquo;</span>
-                        <span className="sr-only">Previous</span>
-                    </button>
-                </li>
-                <li className="page-item">
-                    <Link className="page-link" to="/page/1">1</Link>
-                </li>
-                <li className="page-item">
-                    <button
-                        className="page-link"
-                        aria-label="Next"
-                    >
-                        <span aria-hidden="true">&raquo;</span>
-                        <span className="sr-only">Next</span>
-                    </button>
-                </li>
-            </ul>
-        </nav>
+        <Pagination />
     </>
 );
 
---
+++ src/features/pagination/Pagination.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Pagination: React.FC = () => {
+    return (
+        <nav
+            className="d-flex justify-content-center mt-2"
+            aria-label="Page navigation example"
+        >
+            <ul className="pagination text-center">
+                <li className="page-item disabled">
+                    <button className="page-link" aria-label="Previous">
+                        <span aria-hidden="true">&laquo;</span>
+                        <span className="sr-only">Previous</span>
+                    </button>
+                </li>
+                <li className="page-item">
+                    <Link className="page-link" to="/page/1">1</Link>
+                </li>
+                <li className="page-item">
+                    <button
+                        className="page-link"
+                        aria-label="Next"
+                    >
+                        <span aria-hidden="true">&raquo;</span>
+                        <span className="sr-only">Next</span>
+                    </button>
+                </li>
+            </ul>
+        </nav>
+    );
+};
+
+export default Pagination;
