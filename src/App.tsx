6fcb7683448a65c0e7f56ae0e7f3c615476a627b
stasyaner <stasyaner@hotmail.com>
2020-10-14 12:05:30 +0200

    App: don't use path for router

--- src/App.tsx
+++ src/App.tsx
@@ -3,29 +3,24 @@ import {
     BrowserRouter as Router,
     Switch,
     Route,
-    useRouteMatch,
 } from "react-router-dom";
 import Home from "./Home";
 import User from "./User";
 import { Container } from "react-bootstrap";
 
-const App: React.FC = () => {
-    const { path } = useRouteMatch();
-
-    return (
-        <Container className="my-5">
-            <Router>
-                <Switch>
-                    <Route exact path="/">
-                        <Home />
-                    </Route>
-                    <Route path={`${path}/:username`}>
-                        <User />
-                    </Route>
-                </Switch>
-            </Router>
-        </Container>
-    );
-};
+const App: React.FC = () => (
+    <Container className="my-5">
+        <Router>
+            <Switch>
+                <Route exact path="/">
+                    <Home />
+                </Route>
+                <Route path="/user/:username">
+                    <User />
+                </Route>
+            </Switch>
+        </Router>
+    </Container>
+);
 
 export default App;
