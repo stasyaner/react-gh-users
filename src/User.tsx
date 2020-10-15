22d35496b33425c596ae28267fd8c992030e4e49
stasyaner <stasyaner@hotmail.com>
2020-10-15 11:55:01 +0200

    User: go back btn

--- src/User.tsx
+++ src/User.tsx
@@ -1,12 +1,24 @@
 import React from "react";
 import {
     useParams,
+    useHistory,
 } from "react-router-dom";
 
 const User: React.FC = () => {
+    const history = useHistory();
     const { username } = useParams<{ username: string }>();
 
-    return <h3>user</h3>;
+    return (
+        <>
+            <button
+                className="btn btn-link px-0"
+                onClick={(): void => { history.goBack(); }}
+            >
+                Go back
+            </button>
+            <h3>Hi, {username} !</h3>
+        </>
+    );
 };
 
 export default User;
